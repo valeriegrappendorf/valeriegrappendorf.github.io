089af3315a689c57ace5b12ed3aa335a95b0b5eb
Grappendorf Valerie (INST-AGR/ESA) <grv1wa3@bosch.com>
2019-07-23 17:29:27 +0200

    up

---
+++ jquery.js
@@ -0,0 +1,112 @@
+ //Scroll UX Navbar
+ var t1 = $("#1").offset().top;
+ var t2 = $("#2").offset().top;
+ var t3 = $("#3").offset().top;
+ var t4 = $("#4").offset().top;
+ var t5 = $("#5").offset().top;
+ var t6 = $("#6").offset().top;
+
+
+   
+ $(document).scroll(function () {
+   if ($("#uxnav1").height() + $(this).scrollTop() > t1) {
+     $("#uxnav1").css("font-weight", "500");
+     $("#uxnav1").css("color", "black");
+
+     $("#uxnav2").css("font-weight", "300");
+     $("#uxnav2").css("color", "grey");
+     $("#uxnav3").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav4").css("font-weight", "300");
+     $("#uxnav4").css("color", "grey");
+     $("#uxnav5").css("font-weight", "300");
+     $("#uxnav5").css("color", "grey");
+     $("#uxnav6").css("font-weight", "300");
+     $("#uxnav6").css("color", "grey");
+   }
+ });
+ $(document).scroll(function () {
+   if ($("#uxnav2").height() + $(this).scrollTop() > t2) {
+     $("#uxnav2").css("font-weight", "500");
+     $("#uxnav2").css("color", "black");
+
+     $("#uxnav1").css("font-weight", "300");
+     $("#uxnav1").css("color", "grey");
+     $("#uxnav3").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav4").css("font-weight", "300");
+     $("#uxnav4").css("color", "grey");
+     $("#uxnav5").css("font-weight", "300");
+     $("#uxnav5").css("color", "grey");
+     $("#uxnav6").css("font-weight", "300");
+     $("#uxnav6").css("color", "grey");
+   }
+ });
+ $(document).scroll(function () {
+   if ($("#uxnav3").height() + $(this).scrollTop() > t3) {
+     $("#uxnav3").css("font-weight", "500");
+     $("#uxnav3").css("color", "black");
+
+     $("#uxnav1").css("font-weight", "300");
+     $("#uxnav1").css("color", "grey");
+     $("#uxnav2").css("font-weight", "300");
+     $("#uxnav2").css("color", "grey");
+     $("#uxnav4").css("font-weight", "300");
+     $("#uxnav4").css("color", "grey");
+     $("#uxnav5").css("font-weight", "300");
+     $("#uxnav5").css("color", "grey");
+     $("#uxnav6").css("font-weight", "300");
+     $("#uxnav6").css("color", "grey");
+   }
+ });
+ $(document).scroll(function () {
+   if ($("#uxnav4").height() + $(this).scrollTop() > t4) {
+     $("#uxnav4").css("font-weight", "500");
+     $("#uxnav4").css("color", "black");
+
+     $("#uxnav1").css("font-weight", "300");
+     $("#uxnav1").css("color", "grey");
+     $("#uxnav2").css("font-weight", "300");
+     $("#uxnav2").css("color", "grey");
+     $("#uxnav3").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav5").css("font-weight", "300");
+     $("#uxnav5").css("color", "grey");
+     $("#uxnav6").css("font-weight", "300");
+     $("#uxnav6").css("color", "grey");
+   }
+ });
+ $(document).scroll(function () {
+   if ($("#uxnav5").height() + $(this).scrollTop() > t5) {
+     $("#uxnav5").css("font-weight", "500");
+     $("#uxnav5").css("color", "black");
+
+     $("#uxnav1").css("font-weight", "300");
+     $("#uxnav1").css("color", "grey");
+     $("#uxnav2").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav3").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav4").css("font-weight", "300");
+     $("#uxnav4").css("color", "grey");
+     $("#uxnav6").css("font-weight", "300");
+     $("#uxnav6").css("color", "grey");
+   }
+ });
+   $(document).scroll(function () {
+   if ($("#uxnav6").height() + $(this).scrollTop() > t6) {
+     $("#uxnav6").css("font-weight", "500");
+     $("#uxnav6").css("color", "black");
+
+     $("#uxnav1").css("font-weight", "300");
+     $("#uxnav1").css("color", "grey");
+     $("#uxnav2").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav3").css("font-weight", "300");
+     $("#uxnav3").css("color", "grey");
+     $("#uxnav4").css("font-weight", "300");
+     $("#uxnav4").css("color", "grey");
+     $("#uxnav5").css("font-weight", "300");
+     $("#uxnav5").css("color", "grey");
+   }
+ });
