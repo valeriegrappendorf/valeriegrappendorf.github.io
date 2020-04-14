509bc3b0f9f598e3b2df8ff35112dcfba3f88460
Valerie Grappendorf <mac@Valeries-Macbook.local>
2020-04-14 14:01:52 +0200

    index2

--- jqueryindex.js
+++ jqueryindex.js
@@ -62,4 +62,25 @@ $(function() {
 
 
 
+  //Landing Page iPad & Handy Animation
+    $(function () {
+            var i = 0;
+            $('#background2').css("background-image", "url(../img/home/" + images[i] + ")");
+            setInterval(function () {
+                i++;
+                if (i == images.length) {
+                    i = 0;
+                }
+    //             $('#responsive').fadeOut("slow", function () {
+    //                 $(this).css("background-image", "url(../img/home/" + images[i] + ")");
+    //                 $(this).fadeIn("slow");
+    //             });
+                      $('#background2').css("background-image", "url(../img/home/" + images[i] + ")");
+            }, 1500);
+        });
+  
+  
+  
+  
+  
 
