b69de90cf332cce3ca40f2e54a613c5eb0865512
Valerie Grappendorf <mac@Valeries-Macbook.local>
2020-04-19 17:12:40 +0200

    Final

--- jqueryindex.js
+++ jqueryindex.js
@@ -1,65 +1,10 @@
 //Landing Page Image Change Destkop View
 
-$(function() {
-  $('.text1').hover(function() {
-      $('#background').css('background-image', 'url(../img/home/scooter.png)');
-  })
-});
-$(function() {
-  $('.text2').mouseover(function() {
-      $('#background').css('background-image', 'url(../img/home/smart.png)');
-  })
-});
-$(function() {
-  $('.text3').mouseover(function() {
-      $('#background').css('background-image', 'url(../img/home/post.png)');
-  })
-});
-$(function() {
-  $('.text4').mouseover(function() {
-    $('#background').css('background-image', 'url(../img/home/bosch.png)');     
-    })  
-  });
-$(function() {
-  $('.text5').mouseover(function() {
-    $('#background').css('background-image', 'url(../img/home/opto.png)');     
-    })  
-  });
-$(function() {
-  $('.text6').mouseover(function() {
-      $('#background').css('background-image', 'url(../img/home/lucie.png)');
-  })
-});
 // $(function() {
-  //   $('.text5').mouseover(function() {
-  //     $('#background').animate({ opacity: 0 }, { duration: 100 });
-  //     setTimeout(function() {
-  //       $('#background').delay(200).css('background-image', 'url(../img/home/opto.png)').animate({ opacity: 1 }, { duration: 300 });
-  //      }, 100);
-  //     })  
-  //   });
-
-
-
-
-
-//Landing Page iPad & Handy Animation
-  $(function () {
-          var i = 0;
-          $('#responsive').css("background-image", "url(../img/home/" + images[i] + ")");
-          setInterval(function () {
-              i++;
-              if (i == images.length) {
-                  i = 0;
-              }
-  //             $('#responsive').fadeOut("slow", function () {
-  //                 $(this).css("background-image", "url(../img/home/" + images[i] + ")");
-  //                 $(this).fadeIn("slow");
-  //             });
-                    $('#responsive').css("background-image", "url(../img/home/" + images[i] + ")");
-          }, 1500);
-      });
-
+//   $('.text1').hover(function() {
+//       $('#background').css('background-image', 'url(../img/home/scooter.png)');
+//   })
+// });
 
 
   //Landing Page iPad & Handy Animation
